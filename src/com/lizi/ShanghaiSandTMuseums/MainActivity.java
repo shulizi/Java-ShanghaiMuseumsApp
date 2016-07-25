56d588347a17a514aec630e414b514d3b83e6f38
shulizi <464592183@qq.com>
2016-07-25 10:44:08 +0800

    上传第一个版本

---
+++ src/com/lizi/ShanghaiSandTMuseums/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lizi.ShanghaiSandTMuseums;
+
+import com.lizi.shanghaisandtmuseums.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
